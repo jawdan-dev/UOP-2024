1bda37a5f8aeb9671ab4fce8faa691e0a66a5ab3
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-06-24 16:55:34 +0100

    Script cleanup + coyote time added.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 @export_category("Movement Properties")
 @export var movementSpeed : float = 5.0;
 @export var movementJumpImpulse : float = 6.0;
+@export var movementJumpCoyoteTime : float = 0.2;
+var movementTimeSinceLastJump : float = 0.0;
+var movementTimeSinceLastGrounded : float = 0.0;
 @export var movementGravityAcceleration : float = -9.8;
 var movementGravity : float = 0;
 
@@ -14,12 +17,25 @@ var cameraAngle : Vector2 = Vector2.ZERO;
 
 @export_category("Other")
 
-func _process(delta):
+func _process(delta):		
+	# Handle gravity.
+	handleGravity(delta);
+	# Handle movement.
+	handleMovement(delta);
+	
+	# Handle camera.
+	handleCamera(delta);
+	
+	# Movement.
+	var totalMovement : Vector3 = ((forwardMovement + sideMovement) * movementSpeed) + verticalMovement;
+	velocity = totalMovement;	
+	# Move!
+	move_and_slide();
+
+################################################################################
+
+func handleCamera(delta):
 	# Get player input.
-	var movementInput : Vector2 = Vector2(
-		Input.get_axis("player_movement_rightward", "player_movement_leftward"),
-		Input.get_axis("player_movement_backward", "player_movement_forward")
-	);
 	var cameraInput : Vector2 = Vector2(
 		Input.get_axis("player_camera_right", "player_camera_left"),
 		Input.get_axis("player_camera_up", "player_camera_down") # TODO: Inverted view axis.
@@ -27,6 +43,12 @@ func _process(delta):
 	
 	# Move camera.
 	cameraAngle += cameraInput * delta * cameraLookSpeed;
+	
+	# Clamp angles.
+	const cameraMaxExtent = TAU * 0.25 * 0.9;
+	cameraAngle.y = clampf(cameraAngle.y, -cameraMaxExtent, cameraMaxExtent);
+	
+	# Update camera object.
 	if (cameraObject): 
 		cameraObject.look_at_from_position(
 			# Camera location.
@@ -38,42 +60,63 @@ func _process(delta):
 			# Target object.
 			global_position
 		);
-		
-	# Get movement axis.
-	var forwardMovement : Vector3 = Vector3(
+
+
+################################################################################
+
+var forwardMovement : Vector3;
+var sideMovement : Vector3;
+func handleMovement(delta):
+	# Get player input.
+	var movementInput : Vector2 = Vector2(
+		Input.get_axis("player_movement_rightward", "player_movement_leftward"),
+		Input.get_axis("player_movement_backward", "player_movement_forward")
+	);
+	
+	# Handle forward movement.
+	forwardMovement = Vector3(
 		sin(cameraAngle.x),
 		0,
 		cos(cameraAngle.x)
 	) * movementInput.y;
-	var sideMovement : Vector3 = Vector3(
+	
+	# Handle side movement.
+	sideMovement = Vector3(
 		cos(cameraAngle.x),
 		0,
 		-sin(cameraAngle.x)
 	) * movementInput.x;
+
+################################################################################
+
+var verticalMovement : Vector3;
+func handleGravity(delta):
+	# Handle gravity variables.
+	movementTimeSinceLastJump += delta;
+	movementTimeSinceLastGrounded += delta;
 		
 	# Update gravity.	
 	var gravityFactor : float = movementGravityAcceleration * 0.5 * delta;
 	
-	# Handle jump / floor state.
-	if (is_on_floor()):
-		if (Input.is_action_just_pressed("player_movement_jump")):
-			movementGravity = movementJumpImpulse;
-		else:
-			movementGravity = 0.0;
-			gravityFactor = 0.0;
+	# Handle floor factor.
+	if (is_on_floor()): 
+		movementTimeSinceLastGrounded = 0.0;
+		movementGravity = 0.0;
+		gravityFactor = 0.0;
+	
+	# Handle jump.
+	if (Input.is_action_just_pressed("player_movement_jump") && \
+		movementTimeSinceLastGrounded < movementJumpCoyoteTime && \
+		movementTimeSinceLastGrounded < movementTimeSinceLastJump):
+		# Jump!
+		movementTimeSinceLastJump = 0.0;
+		movementGravity = movementJumpImpulse;
 	
 	# Use gravity.	
 	movementGravity += gravityFactor;
-	var verticalMovement : Vector3 = Vector3(
+	verticalMovement = Vector3(
 		0,
 		movementGravity,
 		0
 	);
 	movementGravity += gravityFactor;
-	
-	# Movement.
-	var movement : Vector3 = ((forwardMovement + sideMovement) * movementSpeed) + verticalMovement;
-	
-	# Move.
-	velocity = Vector3(movement.x, movement.y, movement.z);	
-	move_and_slide();
